b6675002d3cf781a184f1b9f8b943ce96f5df303
alva-projects <alva.lundberg@hotmail.se>
2026-05-05 09:46:03 +0200

    Add tests and GitHub actions workflow

---
+++ src/coffeeshop.js
@@ -0,0 +1,35 @@
+const createDrink = (type) => {
+    const menu = {
+        latte: 45,
+        espresso: 30,
+        cappuccino: 50,
+        "iced coffee": 20
+    }
+    return {
+        type: type,  
+        price: menu[type]
+    }
+}
+
+const prepareOrder = (drink) => {
+    console.log(`Preparing ${drink.type}...`)
+    return Math.floor(Math.random() * 1000)
+}
+
+const processPayment = (ticketNumber, amount) => {
+    if(!ticketNumber) {
+        throw new Error ("No ticket number provided")
+    }
+    console.log(`Payment of ${amount} SEK is processed. Your ticket number is ${ticketNumber}.`)
+    return true
+}
+
+const orderDrink = (drinkType) => {
+    const drink =  (drinkType);
+    const ticket = prepareOrder(drink);
+    return processPayment(ticket, drink.price)
+}
+
+console.log(createDrink(orderDrink("juice")));
+
+module.exports = { createDrink, prepareOrder, orderDrink, processPayment };
